034b6b52784d4e143097b37db8cdf3b38a68d23e
Monica  Karasek <mlkarasek@gmail.com>
2017-10-28 02:02:57 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,14 +4,6 @@ class Student < User
     @knowledge = []
   end
 
-  def first_name
-    @first_name
-  end
-
-  def last_name
-    @last_name
-  end
-
   def learn(knowledge)
     @knowledge << knowledge
   end
